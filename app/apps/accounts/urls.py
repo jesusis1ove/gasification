bc909e6f73ee19e40c7b090955eae3ad6d56de36
y.chernyak <y.chernyak@example.com>
2023-12-04 15:56:20 +0300

    feat: password reset

--- app/apps/accounts/urls.py
+++ app/apps/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -16,4 +16,6 @@ urlpatterns = [
 urlpatterns += [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path(r'password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset'))
 ]
